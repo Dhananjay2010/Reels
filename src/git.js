a35fcebba721d2d8ce4e94ce9d698aada85a78b2
Dhananjay <dhananjay.rtr@gmail.com>
2021-09-24 11:30:45 +0530

    new

---
+++ src/git.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+
+let Git=()=>{
+
+    let [helle, setHello]=useState(0);
+
+    return(
+        <div>
+            <li>Hello</li>
+        </div>
+    )
+}
+
+export default Git;
